57e33811c972851a23deba4c0737113305cc363a
imerla1 <giorgi1048@yahoo.com>
2022-06-05 17:49:31 +0400

    Implemented append method on ll

--- 04-linked-list/ll.py
+++ 04-linked-list/ll.py
@@ -15,9 +15,21 @@ class LinkedList:
             print(temp.value)
             temp = temp.next
     def append(self, value):
-        # create new node
-        # add node to end
-        ...
+        "create new node add node to the end"
+        ll_node = Node(value) # create new node
+        # check if ll is empty
+        if self.head.value == None:
+            # ll doesn't have any node
+            # so head and tail are both empty 
+            self.head = ll_node
+            self.tail = ll_node
+            self.length = 1
+        else:
+            # LinkedList isn't empty so we need to append new_node at the end of the ll
+            prev_node = self.tail
+            prev_node.next = ll_node
+            self.tail = ll_node
+            self.length += 1
     def prepend(self, value):
         # create new node
         # add node to the beginning
@@ -28,4 +40,10 @@ class LinkedList:
         ...
 
 if __name__ == "__main__":
-    my_ll = LinkedList(4)
+    my_ll = LinkedList(None)
+    my_ll.append(25)
+    my_ll.append(76)
+    my_ll.append(26)
+    my_ll.append(68)
+
+    my_ll.print_list()
