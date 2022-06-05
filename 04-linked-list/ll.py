6e5504a5f91d6a4341321245e77263a1c19f75fc
imerla1 <giorgi1048@yahoo.com>
2022-06-05 18:16:22 +0400

    implmented pop methond on ll

--- 04-linked-list/ll.py
+++ 04-linked-list/ll.py
@@ -3,24 +3,27 @@ class Node:
         self.value = value
         self.next = None
 
+
 class LinkedList:
     def __init__(self, value) -> None:
-        new_node = Node(value) # Initial node
+        new_node = Node(value)  # Initial node
         self.head = new_node
         self.tail = new_node
         self.length = 1
+
     def print_list(self):
         temp = self.head
         while temp is not None:
             print(temp.value)
             temp = temp.next
+
     def append(self, value):
         "create new node add node to the end"
-        ll_node = Node(value) # create new node
+        ll_node = Node(value)  # create new node
         # check if ll is empty
         if self.head.value == None:
             # ll doesn't have any node
-            # so head and tail are both empty 
+            # so head and tail are both empty
             self.head = ll_node
             self.tail = ll_node
             self.length = 1
@@ -30,14 +33,41 @@ class LinkedList:
             prev_node.next = ll_node
             self.tail = ll_node
             self.length += 1
+
+    def pop(self):
+        "remove last item from ll and return it"
+        if self.head is None:
+            # we don't have any item in ll
+            return False
+        if self.length == 1:
+            # we have only 1 node (head=tail) we can pop it and return
+            return_value = self.head.value
+            self.head.value = None
+            self.head.next = None
+            return return_value
+        else:
+            # we have more than 1 Node
+
+            prev = self.head
+            temp = self.head
+            while temp.next != None:
+                prev = temp
+                temp = temp.next
+            self.tail = prev
+            self.tail.next = None
+            self.length -= 1
+            return temp
+
     def prepend(self, value):
         # create new node
         # add node to the beginning
         ...
+
     def insert(self, index, value):
         # create new node
         # insert node to specific place based on index
         ...
+
 
 if __name__ == "__main__":
     my_ll = LinkedList(None)
@@ -45,5 +75,5 @@ if __name__ == "__main__":
     my_ll.append(76)
     my_ll.append(26)
     my_ll.append(68)
-
-    my_ll.print_list()
+    x = my_ll.pop()
+    # my_ll.print_list()
