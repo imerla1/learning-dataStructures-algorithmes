aaa0c0beb187c1ac9394c608d265fb6ae12aca5e
imerla1 <giorgi1048@yahoo.com>
2022-06-08 12:39:06 +0400

    implemented prepend methond on DLL

--- 05-doubly-linkedd-list/dll.py
+++ 05-doubly-linkedd-list/dll.py
@@ -63,8 +63,30 @@ class DoublyLinkedList:
 
             return ret_value
 
+    def prepend(self, value):
+        # create new node
+        # add node to the beginning
+        new_node = Node(value)
+        if self.length == 0:
+            # our DLL is empty
+            self.head = new_node
+            self.tail = new_node
+            self.length += 1
+        elif self.length == 1:
+            # we have only 1 item in DLL
+            self.head = new_node
+            self.head.next = self.tail
+            self.tail.prev = self.head
+            self.length += 1
+        else:
+            # we have more than 1 node in DLL
+            temp = self.head
+            self.head = new_node
+            temp.prev = self.head
+            self.head.next = temp
+            self.length += 1
+
 
 if __name__ == "__main__":
     x = DoublyLinkedList(5)
     x.append(25)
-    x.append(53)
